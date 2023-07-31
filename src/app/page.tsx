7997092739343f846fb1a3eb3ad1a2cde31c908b
MarlonDeOcampo <jezozobrado@gmail.com>
2023-07-31 18:43:33 +0800

    Implement dashboard,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,34 @@
-import { Badge } from "@/components/ui/badge";
-import { prisma } from "@/lib/db";
+import SignInButton from "@/components/SignInButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return <Badge variant="secondary">Badge</Badge>;
+export default async function Home() {
+  const session = await getAuthSession();
+
+  if (session?.user) {
+    return redirect("/dashboard");
+  }
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to Cheese Quiz!</CardTitle>
+          <CardDescription>
+            Cheese Quiz is a quiz app that allows you to create and share quiz
+            with your friends.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign In with Google." />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
---
+++ src/components/dashboard/HotTopicsCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+type Props = {};
+
+const HotTopicsCard = (props: Props) => {
+  return (
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold">Hot topics</CardTitle>
+        <CardDescription>
+          Click on a topic to start a quiz on it bitch!
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pl-2">word cloud</CardContent>
+    </Card>
+  );
+};
+
+export default HotTopicsCard;
